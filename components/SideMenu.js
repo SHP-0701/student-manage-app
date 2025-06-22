aeb8826047961d75cd859c254fa365b71ffdba3e
SHP <shpark0701@pknu.ac.kr>
2025-06-22 22:31:06 +0900

    /components/SideMenu.js 작업중

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,3 +1,6 @@
+import styles from '@/styles/SideMenu.module.css';
+import { FaUser, FaUserGraduate, FaClipboardList, FaChartBar, FaSignOutAlt } from 'react-icons/fa';
+
 export default function SideMenu() {
     return <aside>사이드 메뉴에용</aside>
 }
