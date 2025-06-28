4c3f3bdfbb368c2bfd840b93f8c77a00e83c1d61
수훈 <shpark0701@pknu.ac.kr>
2025-06-28 13:39:54 +0900

    /SideMenu 에 router.push() 추가

--- components/SideMenu.js
+++ components/SideMenu.js
@@ -1,24 +1,43 @@
 import styles from '@/styles/SideMenu.module.css';
-import { Router } from 'next/router';
-import { FaUser, FaUserGraduate, FaClipboardList, FaChartBar, FaSignOutAlt } from 'react-icons/fa';
+import { useRouter } from 'next/router';
+import {
+  FaUser,
+  FaUserGraduate,
+  FaClipboardList,
+  FaChartBar,
+  FaSignOutAlt,
+} from 'react-icons/fa';
 
-export default function SideMenu({username}) {
-    
-    return (
-        <aside className={styles.sidebar}>
-            <div className={styles.topSection}>
-                <FaUser className={styles.userIcon} />
-                <p className={styles.welcome}>{username} 님 환영합니다.</p>
-                <button className={styles.logout}><FaSignOutAlt />로그아웃</button>
-            </div>
+export default function SideMenu({ username }) {
+  const router = useRouter();
 
-            <nav className={styles.nav}>
-                <ul>
-                    <li><FaUserGraduate />학생 정보</li>
-                    <li><FaClipboardList />출결 기록</li>
-                    <li><FaChartBar />통계</li>
-                </ul>
-            </nav>
-        </aside>
-    )
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.topSection}>
+        <FaUser className={styles.userIcon} />
+        <p className={styles.welcome}>{username} 님 환영합니다.</p>
+        <button className={styles.logout}>
+          <FaSignOutAlt />
+          로그아웃
+        </button>
+      </div>
+
+      <nav className={styles.nav}>
+        <ul>
+          <li onClick={() => router.push('/dashboard/student')}>
+            <FaUserGraduate />
+            학생 정보
+          </li>
+          <li>
+            <FaClipboardList />
+            출결 기록
+          </li>
+          <li>
+            <FaChartBar />
+            통계
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  );
 }
