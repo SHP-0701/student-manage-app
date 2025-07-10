5d157772ef9a1a57b5ef60d81d0542134c803f71
수훈 <shpark0701@pknu.ac.kr>
2025-07-10 17:47:59 +0900

    모달 작업중...

--- components/LoginModal.js
+++ components/LoginModal.js
@@ -1,14 +1,20 @@
-import styles from '@/styles/LoginModal.module.css';
-import { FaExclamationTriangle, FaCheckCircle } from 'react-icons/fa';
+/**
+ * 로그인 실패 시 렌더링 되는 모달
+ */
 
-export default function LoginModal({message, onClose}) {
-    return (
-        <div className={styles.overlay} onClick={onClose}>
-            <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-                <FaExclamationTriangle className={`${styles.icon} ${styles.error}`} />
-                <p>{message}</p>
-                <button className={styles.button} onClick={onClose}>닫기</button>
-            </div>
-        </div>
-    )
+import styles from '@/styles/LoginModal.module.css';
+import { FaExclamationTriangle } from 'react-icons/fa';
+
+export default function LoginModal({ message, onClose }) {
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        <FaExclamationTriangle className={`${styles.icon} ${styles.error}`} />
+        <p>{message}</p>
+        <button className={styles.button} onClick={onClose}>
+          닫기
+        </button>
+      </div>
+    </div>
+  );
 }
