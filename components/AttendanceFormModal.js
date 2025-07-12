404d6ec7ddbe8f972493fb69e4b8ea9147bc317c
수훈 <shpark0701@pknu.ac.kr>
2025-07-12 23:57:51 +0900

    모달 작업중...

---
+++ components/AttendanceFormModal.js
@@ -0,0 +1,5 @@
+/**
+ * 출결 기록 등록 & 수정 모달(AttendanceFormModal)
+ * DB Table(student_attendance) COLUMN
+ * | [id] | [year] | [term] | [stdNum] | [stdName] | [workType] | [stdJob] | [workDate] | [startTime] | [endTime] | [note] | [created_at] | [updated_at] |
+ */
