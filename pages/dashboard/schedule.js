91e07b95e80888467c2512194db5bf3f354b20a1
수훈 <shpark0701@pknu.ac.kr>
2025-07-13 22:47:55 +0900

    근무시간표 작업중...

--- pages/dashboard/schedule.js
+++ pages/dashboard/schedule.js
@@ -1,10 +1,14 @@
 /**
- * '근로 시간표' 메뉴 선택 시 렌더링됨
- * 리스트 형태? 달력 형태? 아직 정해지진 않았는데 일단 리스트 형식으로 가보려고 함.
+ * '근로 시간표' 메뉴
+ * 왼쪽: 캘린더(react-calendar)
+ * 오른쪽: 리스트
  */
 
-import Layout from '@/components/Layout';
-import styles from '@/styles/Schedule.module.css';
+import Layout from "@/components/Layout";
+import styles from "@/styles/Schedule.module.css";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { useState, useEffect } from "react";
 
 export default function SchedulePage() {
   return (
