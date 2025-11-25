126365aed489b28ecdb1050f49881784a586ec90
수훈 <shpark0701@pknu.ac.kr>
2025-11-25 09:55:33 +0900

    react-hot-toast 사용을 위한 _app.js 수정

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '@/styles/globals.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import '@/styles/datepicker-custom.css';
+import { Toaster } from 'react-hot-toast';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Toaster
+        position='bottom-right'
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#333',
+            color: '#fff',
+          },
+          success: {
+            style: {
+              background: '#10b981',
+            },
+          },
+          error: {
+            style: {
+              background: '#ef4444',
+            },
+          },
+        }}
+      />
+    </>
+  );
 }
