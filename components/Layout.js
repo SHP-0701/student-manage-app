52c6c65168212c85d63c4e7c5931f3be732eb70a
수훈 <shpark0701@pknu.ac.kr>
2026-01-05 17:56:23 +0900

    사이드메뉴 활성화 보여주기 위한 Layout.js 수정

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,19 @@
-/**
- * 공통 레이아웃(Layout.js)
- * /SideMenu 랑 /DashboardContent 사용 */
+/*
+=======================================================================================
+  ○ 작성일자: 2026. 01. 05.(월)
+  ○ 페이지명: /components/Layout.js
+  ○ 내용: 페이지 구성 기본틀(왼쪽: 사이드메뉴 / 오른쪽: 해당 메뉴 콘텐츠)
+  ○ 작성자: 박수훈(shpark)
+=======================================================================================
+*/
 
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import SideMenu from './SideMenu';
 import styles from '@/styles/Layout.module.css';
-import { useState, useEffect } from 'react';
 
 export default function Layout({ children }) {
+  const router = useRouter();
   const [userName, setUserName] = useState('');
 
   useEffect(() => {
@@ -16,7 +23,7 @@ export default function Layout({ children }) {
 
   return (
     <div className={styles.layoutWrapper}>
-      <SideMenu username={userName} />
+      <SideMenu username={userName} currentPath={router.pathname} />
       <main className={styles.content}>{children}</main>
     </div>
   );
