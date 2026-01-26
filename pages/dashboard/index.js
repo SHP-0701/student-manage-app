91b0ab2468ddf2fa32824ed3e9dd6c58184deee9
수훈 <shpark0701@pknu.ac.kr>
2026-01-26 16:41:07 +0900

    대시보드 페이지 개선(기능 추가)

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,9 +1,9 @@
 /* 
   ==================================================================================
-  ○ 작성일자: 2026. 01. 05.(월)
-  ○ 페이지명: 근로학생 관리시스템 대시보드(/dashboard/index.js)
-  ○ 내 용: 로그인 후 관리자 대시보드 화면 및 인증(로그인 여부) 체크
-  ○ 작성자: 박수훈(shpark)
+    ○ 작성일자: 2026. 01. 05.(월)
+    ○ 페이지명: 근로학생 관리시스템 대시보드(/dashboard/index.js)
+    ○ 내 용: 로그인 후 관리자 대시보드 화면 및 인증(로그인 여부) 체크
+    ○ 작성자: 박수훈(shpark)
   ================================================================================== 
 */
 
@@ -41,6 +41,33 @@ export default function DashboardPage() {
         <div className={styles.welcomBox}>
           <h2>안녕하세요 {admName}님!</h2>
           <p>오늘도 좋은 하루 되세요!</p>
+        </div>
+
+        {/** 총 근로 인원, 금일 근로인원, 근로확인 대기 인원 카드 영역 */}
+        <div className={styles.statusContainer}>
+          {/** [1] 총 인원 */}
+          <div className={styles.statusCard}>
+            <h3>총 인원</h3>
+            <p className={styles.statusValue}>
+              32<span>명</span>
+            </p>
+          </div>
+
+          {/** [2] 금일 근로 인원 */}
+          <div className={styles.statusCard}>
+            <h3>금일 근로인원</h3>
+            <p className={styles.statusValue}>
+              5<span>명</span>
+            </p>
+          </div>
+
+          {/** [3] 확인 대기 */}
+          <div className={styles.statusCard}>
+            <h3>확인 대기</h3>
+            <p className={`${styles.statusValue} ${styles.alertText}`}>
+              2<span>건</span>
+            </p>
+          </div>
         </div>
       </div>
     </Layout>
